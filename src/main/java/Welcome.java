2396e4596ec8c01b6a986d55888c472af2766e3c
devcom2020 <kwp@tlen.pl>
2023-03-15 14:18:20 +0100

    Initial commit

---
+++ src/main/java/Welcome.java
@@ -0,0 +1,24 @@
+/**
+ Pierwszy program w Java
+ @version 1.0
+ @author TK
+ */
+
+public class Welcome {
+    public static void main(String[] args) {
+
+        final int ROK = 2023;
+
+        var pensja = 100.50;
+        int wiek_100;
+        boolean done,undone;
+        long populacjaZiemi;
+        wiek_100 = 40;
+
+        System.out.println("Witaj Orange!");
+        System.out.println("Witaj Orange po raz drugi!");
+        System.out.println(pensja);
+        System.out.println(wiek_100);
+
+    }
+}
