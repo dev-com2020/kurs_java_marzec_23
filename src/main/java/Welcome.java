bb43151835a6c83bdbc3fefe98c71072136923ee
devcom2020 <kwp@tlen.pl>
2023-03-15 14:57:15 +0100

    dodanie nowych plików

--- src/main/java/Welcome.java
+++ src/main/java/Welcome.java
@@ -15,11 +15,18 @@ public class Welcome {
         boolean done,undone;
         long populacjaZiemi;
         wiek_100 = 40;
+        var x = 10;
+        var y = 2.5;
+        var z = 2;
+        double wynik = x * y/z + x;
+        double w = Math.sqrt(x);
+        System.out.println(w);
 
         System.out.println("Witaj Orange!");
         System.out.println("Witaj Orange po raz drugi!");
         System.out.println(pensja);
         System.out.println(wiek_100);
+        System.out.println(wynik);
 
     }
 }
