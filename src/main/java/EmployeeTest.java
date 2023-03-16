c231bc9748bb2a185205b4c7cd6b5f01a7d3d5fe
dev-com2020 <kwp@tlen.pl>
2023-03-16 15:30:16 +0100

    16.03 15:30

---
+++ src/main/java/EmployeeTest.java
@@ -0,0 +1,45 @@
+import java.time.LocalDate;
+
+/**
+ * Ten program sprawdza działanie klasy Employee
+ *
+ * @author TK
+ * @version 1.1
+ */
+public class EmployeeTest {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Employee {
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(String n, double s, int year, int month, int day) {
+        name = n;
+        salary = s;
+        hireDay = LocalDate.of(year,month,day);
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent){
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+
+}
